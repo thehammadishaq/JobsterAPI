b38a0e913e373347daac5729f51100d99f66a65f
thehammadishaq <thehammadishaq@gmail.com>
2024-06-28 17:05:47 +0500

    Rate Limiter Completed

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,20 @@ const express = require('express')
 const router = express.Router()
 const { register, login, updateUser } = require('../controllers/auth')
 const authenticateUser = require('../middleware/authentication')
-router.post('/register', register)
-router.post('/login', login)
-router.patch('/updateUser', authenticateUser, updateUser)
+const testUser = require('../middleware/testUser')
+
+const rateLimiter = require('express-rate-limit');
+const apiLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1,
+    message: {
+        msg: 'Too many requests from this IP, please try again after 15 minutes',
+    },
+});
+
+
+router.post('/register', apiLimiter, register)
+router.post('/login', apiLimiter, login)
+router.patch('/updateUser', authenticateUser, testUser, updateUser)
 
 module.exports = router
